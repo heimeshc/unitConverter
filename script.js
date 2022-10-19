bd784dabdc69451b8f641ce48e0a6f13f1be8adf
heimesh <heimesh79@gmail.com>
2022-10-19 15:33:41 +0530

    Add responsiveness

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
 const btn = document.querySelector(".btn")
+let inputValue = document.querySelector(".inputBtn").value
 
+
+let num = 0;
+
+baseStr = `${num} Meters = ${x} Feet | ${num} Feet = ${x} Meters`
+
+function meterAndFeet(){
+    const meter = inputValue / 3.28;
+    const feet = inputValue * 3.28;
+}
+
+function literToGallon(){
+    const liters = inputValue * 3.785;
+    const gallons = inputValue / 3.785;
+}
+
+function kilogramsToPounds(){
+    const kilograms = inputValue / 2.205;
+    const pounds = inputValue * 2.205;
+}
